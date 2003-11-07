e25072448c3cce611b94066cdbb1c00616977008
dylan_cuthbert <dylan_cuthbert>
2003-11-07 09:25:03 +0000

    added Department list drop down menu based on Company selection in Owner box.
    This uses Javascript to populate the dropdown menu with the appropriate
    departments for the selected company.

--- addedit.php
+++ addedit.php
@@ -1,4 +1,4 @@
-<?php /* INVENTORY $Id: addedit.php,v 1.31 2003/11/02 17:29:15 dylan_cuthbert Exp $ */
+<?php /* INVENTORY $Id: addedit.php,v 1.1.1.1 2003/11/07 02:10:40 dylan_cuthbert Exp $ */
 
 global $m,$a,$ttl,$category_list,$brand_list;
 
@@ -60,6 +60,29 @@ if (!$canEdit) {
 
 $purchase_date = new CDate( $obj->inventory_purchased );
 
+
+// load up accessible company list
+
+$compsql = "
+SELECT company_id, company_name
+FROM companies
+";
+	
+$company_list = array();
+
+if (($rows = db_loadList( $compsql, NULL )))
+{
+	foreach ($rows as $row)
+	{
+		if ( !getDenyRead( "companies", $row[ 'company_id' ] ) )
+		{
+	/* store it for later use */
+			$company_list[ $row[ "company_id" ] ] = $row;
+		}
+	}
+}
+
+
 // show title block
 
 $title =  $AppUI->_((($inventory_id)?"Edit Inventory Item":"Add Inventory Item"));
@@ -94,6 +117,7 @@ $titleBlock->show();
 <script type="text/javascript" src="<?php echo $AppUI->cfg['base_url'];?>/lib/calendar/lang/calendar-<?php echo $AppUI->user_locale; ?>.js"></script>
 
 <script LANGUAGE="JavaScript">
+<!--		
 
 function delIt()
 {
@@ -102,6 +126,210 @@ function delIt()
 	}
 }
 
+
+var isIE=document.all?true:false;
+var isDOM=document.getElementById?true:false;
+var isNS4=document.layers?true:false;
+		
+/* _w : which ID (1) or (2) */
+/* _h : (h)ide or (s)how */
+function toggleT(_w,_h) {
+  if (isDOM)
+  {
+    if (_h=='s') document.getElementById(_w).style.visibility='visible';
+    if (_h=='h') document.getElementById(_w).style.visibility='hidden';
+  }
+  else if (isIE) {
+    if (_h=='s') eval("document.all."+_w+".style.visibility='visible';");
+    if (_h=='h') eval("document.all."+_w+".style.visibility='hidden';");
+  }
+  else if(isNS4)
+  {
+    if (_h=='s') eval("document.layers['"+_w+"'].visibility='show';");
+    if (_h=='h') eval("document.layers['"+_w+"'].visibility='hide';");
+  }
+}
+
+
+function EnableDisable( _type )
+{
+	eval( "sel = document.forms['editFrm'].inventory_" + _type + ";" );
+	eval( "newsel = document.forms['editFrm'].inventory_new" + _type + ";" );
+	
+    if ( sel.options[ sel.selectedIndex ].value == -1 )
+	{
+		toggleT( "new" + _type, "s" );
+		newsel.disabled = false;
+		newsel.focus();
+    } else {
+		toggleT( "new" + _type, "h" );
+		newsel.disabled = true;
+    }
+    return true;
+}
+
+
+function submitIt()
+{
+	var form = document.editFrm;
+	
+   	form.submit();
+}
+
+function getElementById( _id )
+{
+	if (isDOM)
+	{
+	  return document.getElementById(_id);
+	}
+	else if (isIE)
+	{
+	  eval( "return document.all."+_id+" ;" );
+	}
+	else if(isNS4)
+	{
+	  eval("return document.layers['"+_id+" ;" );
+	}
+}
+
+function emptyList( box )
+{
+	// Set each option to null thus removing it
+	while ( box.options.length ) box.options[0] = null;
+}
+
+// This function assigns new drop down options to the given
+// drop down box from the list of lists specified
+
+function fillList( box, arr, _selected ) {
+	// arr[0] holds the display text
+	// arr[1] are the values
+	
+	if ( typeof _selected == "undefined" ) _selected = 0;
+	
+	box.selectedIndex=0;
+	
+	try
+	{
+		for ( i = 0; i < arr[0].length; i++ ) {
+
+			// Create a new drop down option with the
+			// display text and value from arr
+
+			option = new Option( arr[0][i], arr[1][i] );
+
+			// Add to the end of the existing options
+
+			box.options[box.length] = option;
+			
+			if ( arr[1][i] == _selected)
+			{
+				box.selectedIndex=i;
+			}
+		}
+	}
+	catch( error )
+	{
+		option = new Option( <?php echo '"'.$AppUI->_( "Non Applicable" ).'"'; ?>, 0 );
+		box.options[ box.length ] = option;
+	}
+
+}
+
+// This function performs a drop down list option change by first
+// emptying the existing option list and then assigning a new set
+
+function changeList( box, lists, id, _selected )
+{
+	// Isolate the appropriate list by using the value
+	// of the currently selected option
+	
+	if ( typeof selected == "undefined" ) selected = 0;
+
+	list = lists[box.options[box.selectedIndex].value];
+
+	// Next empty the slave list
+
+	destination =  getElementById( id );
+	emptyList( destination );
+
+	// Then assign the new list values
+
+	fillList( destination, list, _selected );
+}
+
+
+
+
+// create javascript variables to populate the department and users dialogs
+// based on security settings and dynamic form selection
+
+var dept_lists = new Array();
+
+// load up list of departments per company
+// and set them into javascript variables
+
+<?php
+
+$deptsql = "
+SELECT dept_id, dept_name, dept_company
+FROM departments
+";
+
+if (($sql_list = db_loadList( $deptsql, NULL )))
+{
+// create quick index table
+	
+	$dept_list = array();
+	foreach ( $sql_list as $row )
+	{
+		$dept_list[ $row[ "dept_id" ] ] = $row;
+	}
+	
+// create quick index table for departments' companies
+	
+	$dept_company_list = array();
+	reset( $dept_list );
+	foreach( $dept_list as $dept )
+	{
+		$dept_company_list[ $dept[ "dept_company" ] ][] = $dept[ "dept_id" ];
+	}
+	
+// now go through each company in turn
+	
+	reset( $dept_company_list );
+	foreach ( $dept_company_list as $key => $dept_company )
+	{
+		echo "dept_lists[ ".$key." ] = Array(); ";
+		
+		$nametext = "dept_lists[ ".$key." ][0] = new Array( ";
+		$valuetext = "dept_lists[ ".$key." ][1] = new Array( ";
+		
+		$lastval = end($dept_company);
+		reset( $dept_company );
+		
+// and go through each department within that company
+		
+		foreach( $dept_company as $dept_id )
+		{
+			$nametext .= '"'.$dept_list[ $dept_id ][ "dept_name" ].'"';
+			$valuetext .= $dept_id;
+			if ( $lastval != $dept_id )
+			{
+				$nametext .= ", ";
+				$valuetext .= ", ";
+			}
+			
+		}
+		
+		echo $nametext." );\n";
+		echo $valuetext." );\n";
+	}
+}
+
+?>
+
+-->
 </SCRIPT>
 
 
@@ -147,7 +375,7 @@ function delIt()
 			<TR>
 				<TD>
 					<?php echo $AppUI->_( 'Brand' );?>: <BR />
-					<SELECT NAME="inventory_brand" ONCHANGE="EnableDisable( 'brand' );">
+					<SELECT NAME="inventory_brand" ONCHANGE="javascript:EnableDisable( 'brand' );">
 						<?php
 							foreach ($brand_list as $brand)
 							{
@@ -162,7 +390,7 @@ function delIt()
 					</SPAN>
 				<BR /><BR />
 					<?php echo $AppUI->_( 'Category' );?>: <BR />
-					<SELECT NAME="inventory_category" ONCHANGE="EnableDisable( 'category' );">
+					<SELECT NAME="inventory_category" ONCHANGE="javascript:EnableDisable( 'category' );">
 						<?php
 							foreach ($category_list as $category)
 							{
@@ -223,62 +451,19 @@ function delIt()
 			<TR>
 				<TD>
 					<?php echo $AppUI->_( "Company" ); ?>: <BR />
-					<SELECT NAME="inventory_company" CLASS="text">
+					<SELECT NAME="inventory_company" ID="companylist" CLASS="text" ONCHANGE="javascript:changeList( this, dept_lists, 'departmentlist' )">
 					<?php
-						$compsql = "
-						SELECT company_id, company_name
-						FROM companies
-						";
-						
-						if (($rows = db_loadList( $compsql, NULL )))
+						foreach( $company_list as $row )
 						{
-							foreach ($rows as $row)
-							{
-								if ( !getDenyRead( "companies", $row[ 'company_id' ] ) )
-								{
-									$compname = $row["company_name"];
-									echo "<OPTION VALUE='".$row["company_id"].(($row[ "company_id" ] == $obj->inventory_company ) ? "' SELECTED>" : "'>" ).$compname;
-								}
-							}
+							$compname = $row["company_name"];
+							echo "<OPTION VALUE='".$row["company_id"].(($row[ "company_id" ] == $obj->inventory_company ) ? "' SELECTED>" : "'>" ).$compname;
 						}
-				
 					?>
 					</SELECT>
 				</TD>
 				<TD>
 					<?php echo $AppUI->_( "Department" ); ?>: <BR />
-<!--					<INPUT TYPE="TEXT" ID="invdept_id" NAME="inventory_department" VALUE="<?php echo $obj->inventory_department; ?>" HIDDEN />-->
-<!--					<SPAN CLASS="text" ID="invdept_name">--><?php
-/*						if ( $obj->inventory_department )
-						{
-							$deptsql = "
-							SELECT dept_id, dept_name
-							FROM departments
-							WHERE dept_id = {$obj->inventory_department}
-							";
-							$rows = db_loadList( $
-						}
-						else echo $AppUI->_( "click to set" );
-*/					
-					?><!--</SPAN>-->
-					<SELECT NAME="inventory_department" CLASS="text">
-					<?php
-						$deptsql = "
-						SELECT dept_id, dept_name
-						FROM departments
-						";
-						
-						if (($rows = db_loadList( $deptsql, NULL )))
-						{
-							foreach ($rows as $row)
-							{
-								if ( !getDenyRead( "departments", $row[ 'dept_id' ] ) )
-								{
-									echo "<OPTION VALUE='".$row["dept_id"].(($row[ "dept_id" ] == $obj->inventory_department ) ? "' SELECTED>":"'>").$row["dept_name"];
-								}
-							}
-						}
-					?>
+					<SELECT NAME="inventory_department" ID="departmentlist" CLASS="text" >
 					</SELECT>
 				</TD>
 			</TR>
@@ -296,11 +481,17 @@ function delIt()
 						FROM projects
 						";
 						
+						$project_list = array();
+						
 						if (($rows = db_loadList( $usersql, NULL )))
 						{
 							foreach ($rows as $row)
 							{
-								echo "<OPTION VALUE='".$row["project_id"].(($row[ "project_id" ] == $obj->inventory_project ) ? "' SELECTED>":"'>").$row["project_name"];
+								if ( !getDenyRead( "projects", $row[ 'project_id' ] ) )
+								{
+									$project_list[ $row[ "project_id" ] ] = $row;
+									echo "<OPTION VALUE='".$row["project_id"].(($row[ "project_id" ] == $obj->inventory_project ) ? "' SELECTED>":"'>").$row["project_name"];
+								}
 							}
 						}
 					?>
@@ -350,58 +541,11 @@ function delIt()
 
 
 <SCRIPT LANGUAGE="JavaScript">
-		
-		
-var isIE=document.all?true:false;
-var isDOM=document.getElementById?true:false;
-var isNS4=document.layers?true:false;
-		
-/* _w : which ID (1) or (2) */
-/* _h : (h)ide or (s)how */
-function toggleT(_w,_h) {
-  if (isDOM)
-  {
-    if (_h=='s') document.getElementById(_w).style.visibility='visible';
-    if (_h=='h') document.getElementById(_w).style.visibility='hidden';
-  }
-  else if (isIE) {
-    if (_h=='s') eval("document.all."+_w+".style.visibility='visible';");
-    if (_h=='h') eval("document.all."+_w+".style.visibility='hidden';");
-  }
-  else if(isNS4)
-  {
-    if (_h=='s') eval("document.layers['"+_w+"'].visibility='show';");
-    if (_h=='h') eval("document.layers['"+_w+"'].visibility='hide';");
-  }
-}
-
-function EnableDisable( _type )
-{
-	eval( "sel = document.forms['editFrm'].inventory_" + _type + ";" );
-	eval( "newsel = document.forms['editFrm'].inventory_new" + _type + ";" );
-	
-    if ( sel.options[ sel.selectedIndex ].value == -1 )
-	{
-		toggleT( "new" + _type, "s" );
-		newsel.disabled = false;
-		newsel.focus();
-    } else {
-		toggleT( "new" + _type, "h" );
-		newsel.disabled = true;
-    }
-    return true;
-}
-
-
-function submitIt()
-{
-	var form = document.editFrm;
-	
-   	form.submit();
-}
-
-
+<!--
 EnableDisable( 'category' );
 EnableDisable( 'brand' );
+changeList( getElementById( "companylist" ), dept_lists, 'departmentlist', <?php echo $obj->inventory_department; ?> );
 
+-->
 </SCRIPT>
+
